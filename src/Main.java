0fc2f87fe4b431fe8b0d8bf3364858009df2d60f
abhinike <abhishekssinha2003@gmail.com>
2023-01-04 20:04:04 +0530

    Binary heap impplemented

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import PriorityQueue.BinaryHeap;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+//        System.out.println("Hello world!");
+        BinaryHeap<Character> bp = new BinaryHeap<Character>(20);
+
+//       boolean a =  bp.less(3,3);
+//        System.out.println(a);
+        bp.insert('Y');
+        bp.insert('Q');
+        bp.insert('O');
+        bp.insert('I');
+        bp.insert('A');
+        bp.insert('B');
+
+
+
+        System.out.println(bp.delMax());
+        System.out.println(bp.delMax());
+        System.out.println(bp.delMax());
+        System.out.println(bp.delMax());
+        System.out.println(bp.delMax());
+        System.out.println(bp.delMax());
+
     }
 }
---
+++ src/PriorityQueue/MaxPQ.java
@@ -0,0 +1,38 @@
+//package PriorityQueue;
+//
+//public class MaxPQ <key extends Comparable<key>>{
+//
+////    create an empty priority queue
+//     MaxPQ();
+//
+////    create a priority queue with given keys
+//     MapPQ(key[] a);
+//
+//     void insert(key v){}
+//
+//
+////return and remove the largest key
+//    key delMax(key a){
+//         return a;
+//    }
+//
+//    boolean isEmpty(){
+//         return true;
+//    }
+//
+////    return the largest key
+//    key max(){}
+//
+//
+////    number of entries in the priority queue
+//    int size(){}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//}
