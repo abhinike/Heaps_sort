4a2f12558dd82320f7b6e98517fb2d853fe137f3
abhinike <abhishekssinha2003@gmail.com>
2023-01-05 19:39:59 +0530

    heap sort implemented

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,40 @@
 import PriorityQueue.BinaryHeap;
+import PriorityQueue.BinaryHeapSort;
 
 public class Main {
     public static void main(String[] args) {
+//sort testing
+        Comparable[] a = {1,2,3,4,3,2,1};
 
-//        System.out.println("Hello world!");
-        BinaryHeap<Character> bp = new BinaryHeap<Character>(20);
+        BinaryHeapSort.sort(a);
 
-//       boolean a =  bp.less(3,3);
-//        System.out.println(a);
-        bp.insert('Y');
-        bp.insert('Q');
-        bp.insert('O');
-        bp.insert('I');
-        bp.insert('A');
-        bp.insert('B');
+        for (int i = 0 ; i< 7; i++){
+            System.out.println(a[i]);
+        }
 
 
 
-        System.out.println(bp.delMax());
-        System.out.println(bp.delMax());
-        System.out.println(bp.delMax());
-        System.out.println(bp.delMax());
-        System.out.println(bp.delMax());
-        System.out.println(bp.delMax());
+
+
+
+////        System.out.println("Hello world!");
+//        BinaryHeap<Character> bp = new BinaryHeap<Character>(20);
+//
+////       boolean a =  bp.less(3,3);
+////        System.out.println(a);
+//        bp.insert('Y');
+//        bp.insert('Q');
+//        bp.insert('O');
+//        bp.insert('I');
+//        bp.insert('A');
+//        bp.insert('B');
+//
+//
+//        while(!bp.isEmpty()){
+//            System.out.println(bp.delMax());
+//
+//        }
+
 
     }
 }
